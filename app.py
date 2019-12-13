6b4457e8db27cee70347934100e6e7a479a956f9
MichaelSingh7 <mikael.1@live.co.uk>
2019-12-13 16:43:39 +0000

    Updated app.py With Block Content

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'game_database'
+app.config["MONGO_URI"] = 'mongodb+srv://root:t00rUser@myfirstcluster-f87ek.mongodb.net/game_database?retryWrites=true&w=majority'
+
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_reviews')
+def get_reviews():
+    return render_template("reviews.html", reviews=mongo.db.reviews.find())    
 
 
 if __name__ == '__main__':
